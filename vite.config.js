56c8215b3bb4282bc7dbcc56e7f0dea5c04db52c
bokhtier19 <bokhtierelius19@gmail.com>
2025-02-07 16:37:16 +0530

    remove vite thingy

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     base: "/Skip_Leg_Day/",
-    build: {
-        rollupOptions: {
-            external: ["react-router-dom"],
-        },
-    },
 });

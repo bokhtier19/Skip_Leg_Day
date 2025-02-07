d97cc26143cdad4fdbe64d88829c53a982273e94
bokhtier19 <bokhtierelius19@gmail.com>
2025-02-07 16:02:17 +0530

    browserrouter to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import SmoothScrolling from "./components/SmoothScrolling.tsx";
 import AnimaitonProvider from "./context/AnimationContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <HashRouter>
         <AnimaitonProvider>
             <SmoothScrolling>
                 <App />
             </SmoothScrolling>
         </AnimaitonProvider>
-    </BrowserRouter>
+    </HashRouter>
 );

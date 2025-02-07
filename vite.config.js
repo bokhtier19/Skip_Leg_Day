4cf4e35c422202c783b0889d72d3c64d49ff3cf9
bokhtier19 <bokhtierelius19@gmail.com>
2025-02-07 16:15:02 +0530

    donno

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     base: "/Skip_Leg_Day/",
+    build: {
+        rollupOptions: {
+            external: ["react-router-dom"],
+        },
+    },
 });
